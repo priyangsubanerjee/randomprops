ec3f13bb50300cebbb1b91a2017b1daeb76d9611
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-02-18 19:05:54 +0530

    d

--- db/props.js
+++ db/props.js
@@ -14,6 +14,33 @@ const data = [
         "image": host + "/images/cookie_monster.png"
     },
 
+    {
+
+        "id": "3",
+        "name": "Green Koala",
+        "image": host + "/images/green_koala.png"
+    },
+
+    {
+        "id": "4",
+        "name": "Gummy Bear",
+        "image": host + "/images/gummy_bear.png"
+    },
+
+    {
+
+        "id": "5",
+        "name": "Honey Badger",
+        "image": host + "/images/honey_badger.png"
+    },
+
+    {
+
+        "id": "6",
+        "name": "Kitty Cat",
+        "image": host + "/images/kitty_cat.png"
+    }
+
 ]
 
 
