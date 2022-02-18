6095f6beb805e90b3142cf5ee36a6d7967b04cb9
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-02-18 18:46:49 +0530

    i

---
+++ db/props.js
@@ -0,0 +1,21 @@
+const data = [
+
+    {
+        "id": "1",
+        "name": "Chocolate Dinosaur",
+        "image": __dirname + "/images/chocolate_dinosaur.png"
+    },
+    {
+        "id": "2",
+        "name": "Cookie Monster",
+        "image": __dirname + "/images/cookie_monster.png"
+    },
+
+]
+
+
+
+module.exports = {
+    props : data
+}
+
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const props = require('./db/props');
+const all_props = props.props;
+const port = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, './views')));
+
+app.get('/api/random', (req, res) => {
+    
+    const random_prop = all_props[Math.floor(Math.random() * all_props.length)];
+    res.send(random_prop);
+})
+
+
+
+app.get('/api/all', (req, res) => {
+
+    res.send(all_props);
+})
+
+
+
+
+app.listen(port, () => {
+
+    console.log('App listening on port 3001!');
+})
